b1fdf0adc946e3e6f1ce776fe952a92295c9e387
Ajay Singh <singh.ajay@husky.neu.edu>
2019-01-30 03:25:32 -0500

    update init

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,7 +7,8 @@ from flask_login import LoginManager
 app = Flask(__name__)#special __name__==__main__
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 #app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/inventory_app'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/inventory_app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/inventory_app'
 db =SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
